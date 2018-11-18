3ce4a274a2283ca48040574876a8b5d9278c0f2b
jcsou <jcsou@DESKTOP-CDANP1D>
2018-11-18 09:06:20 +0100

    update centos

--- InitServeurDeploy/install_centos.sh
+++ InitServeurDeploy/install_centos.sh
@@ -4,6 +4,9 @@ yum install git
 yum -y install epel-release
 yum install ansible
 
-yum install java-1.8.0-openjdk-devel
+## key authent local
+ssh-keygen -t rsa -b 2048
+cat .ssh/id_rsa.pub > .ssh/authorized_keys
 
 ## INSTALL 
+yum install java-1.8.0-openjdk-devel
